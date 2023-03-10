ab5ae3d541ec1e925367e3a8640ea7d176f0bc6b
Xunzhuo <mixdeers@gmail.com>
2023-03-10 18:48:55 +0800

    refacor: i2gw codes and toolings
    Signed-off-by: Xunzhuo <mixdeers@gmail.com>

--- cmd/i2gw/main.go
+++ cmd/i2gw/main.go
@@ -1,11 +1,11 @@
 /*
-Copyright © 2022 Kubernetes Authors
+Copyright © 2023 Kubernetes Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,31 +13,19 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package cmd
+
+package main
 
 import (
 	"fmt"
 	"os"
 
-	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw"
-	"github.com/spf13/cobra"
+	"github.com/kubernetes-sigs/ingress2gateway/pkg/cmd"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "ingress2gateway",
-	Short: "Convert Ingress manifests to Gateway API manifests",
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := cmd.ParseFlags(args); err != nil {
-			fmt.Printf("Error parsing flags: %v", err)
-		}
-
-		i2gw.Run()
-	},
-}
-
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+func main() {
+	if err := cmd.GetRootCommand().Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
